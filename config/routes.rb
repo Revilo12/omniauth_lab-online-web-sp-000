b7a3100c06629fb421d5bd899bda05aca294bef8
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-27 14:02:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/home'
-  root to: 'welcome#home', as: :home
+  root_path to: 'welcome#home', as: :home
   # Add your routes here
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
